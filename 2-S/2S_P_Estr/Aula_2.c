ee9590ab8d998e6f1f9edf12eb2e20e635d72b3f
daviolsilva <daviolsilva@hotmail.com>
2026-08-18 20:29:02 -0300

    Commit New Lessons

---
+++ 2-S/2S_P_Estr/Aula_2.c
@@ -0,0 +1,24 @@
+//Caso Base  X  Caso Recursivo
+
+//Parte 1: Fatorial
+
+#include <stdio.h>
+
+long int fatorial(long int num);
+int main(){
+    long int n, fat;
+
+
+}
+
+
+
+
+
+// Parte 2: Fibonacci
+
+
+
+
+
+
